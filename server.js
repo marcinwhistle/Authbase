90f4b02456c6b399f561012dea61284d303e01b2
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-18 11:06:24 +0200

    add routes for auth and user

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const passportConfig = require('./config/passport');
 const session = require('express-session');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-app.use(session({ secret: 'secretsecret' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// set handlebars as view engine
 app.engine(
   'hbs',
   hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' })
 );
 app.set('view engine', '.hbs');
 
+// init session mechanism
+app.use(session({ secret: 'anything' }));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,26 +32,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
-
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
