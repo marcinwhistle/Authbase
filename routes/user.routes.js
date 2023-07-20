ce70c5b8fb187ad7f569614d471c4dfe03eb75d1
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-20 11:06:49 +0200

    Add subpages profile and profilesetting and add logout option

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  if (req.user) {
+    res.render('logged');
+  } else res.redirect('/user/no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+  if (req.user) {
+    res.render('profile');
+  } else res.redirect('/user/no-permission');
+});
+
+router.get('/profile/profileSettings', (req, res) => {
+  if (req.user) {
+    res.render('profileSettings');
+  } else res.redirect('/user/no-permission');
+});
+
+router.get('/auth.logout', (req, res) => {
+  req.logout();
+  res.redirect('/user/logged');
 });
 
 module.exports = router;
